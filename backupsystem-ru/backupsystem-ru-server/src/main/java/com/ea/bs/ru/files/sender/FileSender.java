cf2176c562035eea23d5389d4bf9a965931c08a4
Ermal <ermal.aliraj@gmail.com>
2017-03-23 19:55:52 +0100

    RU modification FileSender reply without lookup (extends MessageSenderSingle)

--- backupsystem-ru/backupsystem-ru-server/src/main/java/com/ea/bs/ru/files/sender/FileSender.java
+++ backupsystem-ru/backupsystem-ru-server/src/main/java/com/ea/bs/ru/files/sender/FileSender.java
@@ -13,30 +13,34 @@ import org.apache.commons.logging.LogFactory;
 import com.ea.bs.protocol.files.message.FileMsgInfo;
 import com.ea.bs.protocol.files.message.ru.SendFileMessage;
 import com.ea.bs.ru.config.RuConfig;
+import com.ea.jms.MessageConnection;
 import com.ea.jms.exception.MessageException;
-import com.ea.jms.sender.MessageSender;
+import com.ea.jms.sender.MessageSenderSingle;
 
 
 @Stateless
-public class FileSender extends MessageSender implements FileSenderLocal {
+public class FileSender extends MessageSenderSingle implements FileSenderLocal {
 
 	private static final Log log = LogFactory.getLog(FileSender.class);
 	
 	@EJB(name = "RuConfig")
 	private RuConfig ruConfig;
+	
+	MessageConnection messageConnection;
 
 	@PostConstruct
 	public void setDestinationName(InvocationContext ctx) {
 		//super.destinationName = "jms/queue/scapture";
 		super.destinationName = "scapture";
-		super.messageConnection = ruConfig.getServerConnection();
+		messageConnection = ruConfig.getServerConnection();
 	}
 
 	public void sendFiles(List<FileMsgInfo> list) throws MessageException {
 		log.info("Sending files to the Server");
 		long idRu = ruConfig.getConfigDescriptor().getIdRu();
 		SendFileMessage sendFileMsg = new SendFileMessage(idRu, list);
-		sendAsynchMessage(sendFileMsg);
+		//sendAsynchMessage(messageConnection, sendFileMsg);
+		sendAsynchMessage(messageConnection, sendFileMsg, false);
 		log.info("[RU] "+list.size() + " files correctly sent to the server from RU:"+idRu);
 	}
 
